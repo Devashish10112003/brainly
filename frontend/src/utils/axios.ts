b7f51155f180a2ef30318da4a43ec48fd89914ff
Devashish10112003 <devashishmorwani1011@gmail.com>
2026-01-31 19:54:07 +0530

    fixing the backend url

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
+const backeendURL=import.meta.env.VITE_BACKEND_URL;
 
+console.log(backeendURL);
 console.log()
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: backeendURL,
   withCredentials: true
 });
 
